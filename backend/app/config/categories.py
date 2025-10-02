edd28b3a67d765bec405dd7e9f1c038885ff7c4a
Avinash Raut <avinash.raut@emirates.com>
2025-10-02 10:21:19 +0400

    added category config

---
+++ backend/app/config/categories.py
@@ -0,0 +1,10 @@
+# app/config/categories.py
+
+# Central place for all product categories used in Pinecone filters
+CATEGORIES = [
+    "Fragrance & Beauty",
+    "Electronics",
+    "Comfort",
+    "Food & Beverage",
+    "Duty Free"
+]
